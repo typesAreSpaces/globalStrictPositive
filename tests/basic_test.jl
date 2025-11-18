7cd133e0f056ab1afc0d699adfb05eb874f2755b
Jose Abel Castellanos Joo <jabelcastellanosjoo@unm.edu>
2025-11-18 11:58:13 -0700

    Fix minor bug.

--- tests/basic_test.jl
+++ tests/basic_test.jl
@@ -5,6 +5,8 @@ using .globalStrictPositive
 using DynamicPolynomials
 using MathLink
 
+motzkin(x, y, z) = x^6 + y^4*z^2 + y^2*z^4 - 3x^2*y^2*z^2;
+
 function basic_test()
   g = -x^4 - x^2*y^2 + 2*x^2*y + x*y^2 - y^2
   g = 10 - x^2 - y^4 - z^8
